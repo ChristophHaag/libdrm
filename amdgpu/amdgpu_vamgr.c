3343ea0674363a7587a9898f9caa34631b0c45a2
Alex Xie <AlexBin.Xie@amd.com>
2015-11-03 15:52:57 -0500

    amdgpu: vamgr can be a struct instead of a pointer
    vamgr is an integral part of amdgpu_device. We don't need to callac and free it.
    This can save CPU time. Reduce heap fragment.
    
    Change-Id: Ib5ca9e93d007370d2d746aea2c21c2f91aefa3c2
    Signed-off-by: Alex Xie <AlexBin.Xie@amd.com>
    Reviewed-by: Christian König <christian.koenig@amd.com>

--- amdgpu/amdgpu_vamgr.c
+++ amdgpu/amdgpu_vamgr.c
@@ -337,7 +337,7 @@ int amdgpu_svm_vamgr_init(struct amdgpu_device *dev)
 		if (!vamgr_svm.valid)
 			return -ENOSPC;
 
-		start = amdgpu_vamgr_find_va(dev->vamgr,
+		start = amdgpu_vamgr_find_va(&dev->vamgr,
 			vamgr_svm.va_max - vamgr_svm.va_min,
 			dev->dev_info.virtual_address_alignment, vamgr_svm.va_min);
 
@@ -367,7 +367,7 @@ int amdgpu_svm_vamgr_init(struct amdgpu_device *dev)
 	 */
 	for (base_required = end - size; base_required >= min_base_required;
 		base_required -= step) {
-		start = amdgpu_vamgr_find_va(dev->vamgr, size,
+		start = amdgpu_vamgr_find_va(&dev->vamgr, size,
 						dev->dev_info.virtual_address_alignment, base_required);
 		if (start != base_required)
 			continue;
@@ -384,12 +384,12 @@ int amdgpu_svm_vamgr_init(struct amdgpu_device *dev)
 			/* Probably there is no space in this process's address space for
 			   such size of SVM range. This is very rare for 64 bit CPU.
 			*/
-			amdgpu_vamgr_free_va(dev->vamgr, start, size);
+			amdgpu_vamgr_free_va(&dev->vamgr, start, size);
 			ret = -ENOMEM;
 			break;
 		} else { /* cpu_address != (void *)start */
 			/* This CPU VM address (start) is not available*/
-			amdgpu_vamgr_free_va(dev->vamgr, start, size);
+			amdgpu_vamgr_free_va(&dev->vamgr, start, size);
 			munmap(cpu_address, size);
 			base_required -= step;
 		}
